ff2a25e62cabd5e0a0e66f8166b36cc7ea85a99d
Daniel <Daniel@LAPTOP-IV3UDAB9>
2022-11-04 14:03:55 +0530

    secndcomit

---
+++ TestingA/src/test/java/TestingA/Manda.java
@@ -0,0 +1,14 @@
+package TestingA;
+
+public class Manda {
+
+	public static void main(String[] args) {
+
+	System.out.println("srinivas");
+	System.out.println("srinivas");
+	System.out.println("srinivas");
+	System.out.println("srinivas");
+
+	}
+
+}
